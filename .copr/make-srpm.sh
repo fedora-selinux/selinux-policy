ed45beabaf0f1af49d473c737ccfa5b4a3f72aa3
Ondrej Mosnacek <omosnace@redhat.com>
2020-11-27 17:12:09 +0100

    Add SRPM build scripts for COPR
    Add .copr/Makefile so that COPR can use it to build a SRPM automatically
    from a git branch.
    
    It is also possible to build the SRPM locally:
    
        $ make -C .copr srpm
        [...]
        $ ls *.src.rpm
        selinux-policy-3.14.7-9.20201127.d277d84.fc34.src.rpm
    
    Signed-off-by: Ondrej Mosnacek <omosnace@redhat.com>

---
+++ .copr/make-srpm.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -ex
+
+outdir="$1"; shift
+
+dirname="$(dirname "$0")"
+
+DISTGIT_URL=https://src.fedoraproject.org/rpms/selinux-policy
+DISTGIT_REF=master
+
+CONTAINER_URL=https://github.com/containers/container-selinux
+EXPANDER_URL=https://github.com/fedora-selinux/macro-expander
+
+rpm -q rpm-build git-core || dnf install -y rpm-build git-core
+
+base_head_id="$(git -C "$dirname/.." rev-parse HEAD)"
+base_short_head_id="${base_head_id:0:7}"
+base_date="$(git show -s --format=%cs HEAD | tr -d -)"
+
+tmpdir="$(mktemp -d)"
+
+trap 'rm -rf "$tmpdir"' EXIT
+
+container_dir="$tmpdir/container-selinux"
+expander_dir="$tmpdir/macro-expander"
+rpmbuild_dir="$tmpdir/rpmbuild"
+distgit_dir="$tmpdir/rpmbuild/SOURCES"
+
+mkdir -p "$distgit_dir"
+
+git clone --single-branch --depth 1 "$CONTAINER_URL" "$container_dir"
+git clone --single-branch --depth 1 "$EXPANDER_URL" "$expander_dir"
+git clone -b "$DISTGIT_REF" --single-branch --depth 1 "$DISTGIT_URL" "$distgit_dir"
+
+git -C "$dirname/.." archive --prefix="selinux-policy-$base_head_id/" --format tgz HEAD \
+	>"$distgit_dir/selinux-policy-$base_short_head_id.tar.gz"
+
+tar -C "$container_dir" -czf "$distgit_dir/container-selinux.tgz" \
+	container.if container.te container.fc
+
+cp "$expander_dir/macro-expander.sh" "$distgit_dir/macro-expander"
+
+(
+	cd "$distgit_dir"
+	sed -i "s/%global commit [^ ]*$/%global commit $base_head_id/" selinux-policy.spec
+	sed -i "s/%{?dist}/.$base_date.$base_short_head_id%{?dist}/" selinux-policy.spec
+	rm -f sources
+	rpmbuild --define "_topdir $rpmbuild_dir" -bs selinux-policy.spec
+)
+
+cp "$rpmbuild_dir/SRPMS/"*.src.rpm "$outdir"
