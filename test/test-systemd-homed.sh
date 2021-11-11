1bd75df45ecacfd3d4ab7d5573d4d3e3882d4076
Markus Linnala <markus.linnala@gmail.com>
2021-11-11 20:53:24 +0200

    test: add test/test-systemd-homed.sh

--- test/test-systemd-homed.sh
+++ test/test-systemd-homed.sh
@@ -2,14 +2,17 @@
 
 set -ex
 
-if false; then
+if true; then
 
-    sudo homectl remove testuser
+    sudo homectl deactivate-all || :
+    sudo homectl remove testuser || :
 
-    sudo rm -rf /var/lib/systemd/home /run/systemd/home /run/systemd/user-home-mount
+    sudo rm -rf /run/cryptsetup /var/lib/systemd/home /run/systemd/home /run/systemd/user-home-mount
 
     sudo systemctl restart systemd-homed
+    sudo restorecon -nvR /run /var/lib/systemd /usr/lib/systemd
 
+    echo "OK> "
     read
 
 fi
@@ -17,7 +20,7 @@ fi
 sudo homectl deactivate-all || :
 sudo homectl remove testuser || :
 sudo homectl list
-sudo homectl create testuser --real-name="Test User" --disk-size=1G --fs-type=xfs --storage=luks  --timezone=Europe/Helsinki --language=fi_FI.utf8
+sudo homectl create testuser --real-name="Test User" --disk-size=1G --fs-type=btrfs --storage=luks  --timezone=Europe/Helsinki --language=fi_FI.utf8
 # This is missing from homectl create actions so filesystem has only unlabeled_t
 sudo homectl with testuser -- restorecon -vFR /home/testuser
 sudo homectl list
@@ -25,6 +28,9 @@ sudo homectl list
 sudo homectl with testuser -- bash -c 'ls -laZ;id -a;iostat;restorecon -nvR /run /var/lib/systemd /usr/lib/systemd; grep /run /proc/mounts'
 
 sudo homectl activate testuser
+
+echo "Login and then test> "
+read
 
 sudo homectl inspect testuser
 ls -laZ /home/testuser.home
@@ -48,14 +54,17 @@ sudo homectl deactivate-all
 
 sudo homectl remove testuser
 
-if false; then
+if true; then
 
+    echo "OK> "
     read
 
     # for s in luks fscrypt directory subvolume cifs; do
     for s in luks directory subvolume; do
         for f in xfs ext4 btrfs; do
             sudo homectl create testuser --real-name="Test User" --disk-size=1G --fs-type="$f" --storage="$s"  --timezone=Europe/Helsinki --language=fi_FI.utf8
+            # This is missing from homectl create actions so filesystem has only unlabeled_t
+            sudo homectl with testuser -- restorecon -vFR /home/testuser
             sudo homectl remove testuser
         done
     done
